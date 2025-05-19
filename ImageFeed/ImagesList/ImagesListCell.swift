7bcc7c3a7fdc5d02caf321ddb50a679f002228db
NatAn <andrei.v.0204@gmail.com>
2025-05-19 18:53:33 +0300

    Добавлены констрейнты к ячейке

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -3,4 +3,12 @@ import UIKit
 final class ImagesListCell: UITableViewCell {
     
     static let reuseIdentifier = "ImagesListCell"
+    
+    
+    @IBOutlet private var cellImage: UIImageView!
+    
+    @IBOutlet private var likeButton: UIButton!
+    
+    @IBOutlet private var dateLabel: UILabel!
+    
 }
