a8e04df90edb552b46632ef5ef4e19fd7ae13f05
NatAn <andrei.v.0204@gmail.com>
2025-05-23 18:55:18 +0300

    Завершил работу по навигации

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -84,9 +84,7 @@ extension ImagesListViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard let image = UIImage(named: photosName[indexPath.row]) else {
-            return 0
-        }
+        guard let image = UIImage(named: photosName[indexPath.row]) else {return 0}
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
         let imageWidth = image.size.width
--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -6,19 +6,19 @@ final class SingleImageViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBAction private func didTapBackButton() {
+            dismiss(animated: true, completion: nil)
+        }
+    
     // MARK: - Public Properties
     
-    var image: UIImage? {
-        didSet {
-            guard isViewLoaded else { return }
-            imageView.image = image
-        }
-    }
+    var image: UIImage?
     
     // MARK: - View Life Cycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         imageView.image = image
     }
 }
