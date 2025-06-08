53f38fa9b61bc9cdccd199a69de7ff9729042ef3
NatAn <andrei.v.0204@gmail.com>
2025-06-08 17:54:05 +0300

    Произведена подготовка для работы с unsplash

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "6N2K0aIUbhtoNEDq-BihN451nNg7lvvZLEqUZHYjdYw"
+    static let secretKey = "67vgSyv0gP2i3tsT22gRX5HNYPsZk65JKSsgfgR_FlY"
+    static let redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
