3ab80185e987309411e167fda4b0ac2d808a8655
NatAn <andrei.v.0204@gmail.com>
2025-06-09 15:59:00 +0300

    Добавлен Auth & WebView контроллеры

---
+++ ImageFeed/Autorisation/AuthViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    // MARK: - Public Properties
+    
+    static let showWebViewSegueIdentifier = "ShowWebView"
+    
+  
+}
