31c15d08e20a739eae9e953daa1b10d9594df53b
NatAn <andrei.v.0204@gmail.com>
2025-06-28 19:06:13 +0300

    Для WebViewViewController изменен KVO

--- ImageFeed/Autorisation/WebViewViewController.swift
+++ ImageFeed/Autorisation/WebViewViewController.swift
@@ -16,49 +16,35 @@ final class WebViewViewController: UIViewController {
     // MARK: - Public Properties
     
     weak var delegate: WebViewViewControllerDelegate?
+    // MARK: - Private Properties
     
+    private var estimatedProgressObservation: NSKeyValueObservation?
     // MARK: - IB Outlets
     
     @IBOutlet private var progressView: UIProgressView!
     @IBOutlet private var webView: WKWebView!
-    
     // MARK: - Actions
     
     @IBAction private func didTapBackButton(_ sender: Any?) {
         dismiss(animated: true, completion: nil)
     }
-    
     // MARK: - View Life Cycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         webView.navigationDelegate = self
+        estimatedProgressObservation = webView.observe(
+            \.estimatedProgress,
+             options: [],
+             changeHandler: { [weak self] _, _ in
+                 guard let self else { return }
+                 self.updateProgress()
+             })
         loadAuthView()
     }
-    // MARK: - KVO
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        webView.addObserver(
-            self,
-            forKeyPath: #keyPath(WKWebView.estimatedProgress),
-            options: .new,
-            context: nil)
-        updateProgress()
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
-    }
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == #keyPath(WKWebView.estimatedProgress) {
-            updateProgress()
-        } else {
-            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
-        }
+    deinit {
+        estimatedProgressObservation?.invalidate()
     }
     // MARK: - Private Methods
     
