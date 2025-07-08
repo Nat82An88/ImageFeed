10ec973ad8bd6d52a2f56f111f5962c6c3665547
NatAn <andrei.v.0204@gmail.com>
2025-07-08 15:53:12 +0300

    Настройка TabBarController в коде

---
+++ ImageFeed/TabBarController/TabBarController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let imagesListViewController = storyboard.instantiateViewController(
+            withIdentifier: "ImagesListViewController"
+        )
+        let profileViewController = storyboard.instantiateViewController(
+            withIdentifier: "ProfileViewController"
+        )
+        self.viewControllers = [imagesListViewController, profileViewController]
+    }
+    
+}
