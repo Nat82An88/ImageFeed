40c6d2cb006215a690fa9788b2d9ed888cc806b6
NatAn <andrei.v.0204@gmail.com>
2025-08-03 19:36:17 +0300

    Написаны оставшиеся тесты

--- ImageFeedUITests/ImageFeedUITests.swift
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -42,10 +42,44 @@ class Image_FeedUITests: XCTestCase {
     }
     
     func testFeed() throws {
+        let tablesQuery = app.tables
         
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        cell.swipeUp()
+        
+        let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
+        XCTAssertTrue(cellToLike.waitForExistence(timeout: 2))
+        
+        cellToLike.buttons["like button off"].tap()
+        cellToLike.buttons["like button on"].tap()
+        
+        XCTAssertTrue(cellToLike.waitForExistence(timeout: 2))
+        
+        cellToLike.tap()
+        
+        let image = app.scrollViews.images.element(boundBy: 0)
+        XCTAssertTrue(image.waitForExistence(timeout: 2))
+        
+        image.pinch(withScale: 3, velocity: 1)
+        image.pinch(withScale: 0.5, velocity: -1)
+        
+        let navBackButtonWhiteButton = app.buttons["nav back button white"]
+        XCTAssertTrue(navBackButtonWhiteButton.waitForExistence(timeout: 2))
+        navBackButtonWhiteButton.tap()
     }
     
     func testProfile() throws {
+        let tabBarButton = app.tabBars.buttons.element(boundBy: 1)
+        XCTAssertTrue(tabBarButton.waitForExistence(timeout: 3))
+        tabBarButton.tap()
         
+        XCTAssertTrue(app.staticTexts["Name Lastname"].waitForExistence(timeout: 2))
+        XCTAssertTrue(app.staticTexts["@username"].waitForExistence(timeout: 2))
+        
+        app.buttons["logout button"].tap()
+        
+        let logoutAlertButton = app.alerts["Bye bye!"].scrollViews.otherElements.buttons["Yes"]
+        XCTAssertTrue(logoutAlertButton.waitForExistence(timeout: 2))
+        logoutAlertButton.tap()
     }
 }
